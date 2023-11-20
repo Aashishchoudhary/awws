c7fdc15d8221785959ec2e313f1493f1bac1389b
Aashishchoudhary <33976894+Aashishchoudhary@users.noreply.github.com>
2023-11-20 17:05:33 +0530

    Update settings.py
    new vol aatch

--- awws/settings.py
+++ awws/settings.py
@@ -128,5 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 import os
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT =../../../newvol
 MEDIA_URL = '/media/' # 'http://myhost:port/media/
