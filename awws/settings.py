059951db93c63711b3b398c263fa25d403e39624
Aashishchoudhary <33976894+Aashishchoudhary@users.noreply.github.com>
2023-11-20 17:39:29 +0530

    Update settings.py
    database

--- awws/settings.py
+++ awws/settings.py
@@ -76,14 +76,11 @@ WSGI_APPLICATION = 'awws.wsgi.application'
 
 DATABASES = {
       'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'dbdata',
-       'USER': 'aashish',
-       'PASSWORD': 'aashish321',
-       'HOST': 'localhost',
-   }
-}
-
+      
+   
+'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+      }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
